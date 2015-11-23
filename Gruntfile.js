57a6bfa209ffa77377ee7a3155c646f4efebf96c
Marion Huebner <mphuebner@gmail.com>
2015-11-22 18:49:07 -0600

    generates express app w/ handlbars and less

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,10 @@ module.exports = function (grunt) {
   grunt.initConfig({
     jshint: {
       options: {
+        client: [
+            'public/javascripts/**/*.js',
+            '!public/javascripts/vendor'
+          ],
         jshintrc: true,
         reporter: require('jshint-stylish'),
         verbose: true
@@ -13,27 +17,44 @@ module.exports = function (grunt) {
     less: {
       compile: {
         files: {
-          'build/css/compiled.css': 'public/css/**/*.less'
+          'build/stylesheets/compiled.css': 'public/stylesheets/**/*.less'
         }
       }
     },
     concat: {
       js: {
         files: {
-          'build/js/bundled.js': 'public/javascripts/**/*.js'
+          'build/javascripts/bundled.js': 'public/javascripts/**/*.js'
         }
       }
     },
     uglify: {
       bundle: {
         files: {
-          'build/js/bundle.min.js': 'build/js/bundle.js'
+          'build/javascripts/bundle.min.js': 'build/javascripts/bundle.js'
         }
       }
+    },
+    sprite: {
+    // Add icons as HTML elements and assign different sprite CSS classes to those elements
+    // CSS is used to “crop” different parts of the image, taking only the relevant part of the image for the desired icon.
+        icons: {
+          src: 'public/images/icons/*.png',
+          destImg: 'build/images/icons.png',
+          destCSS: 'build/css/icons.css'
+      }
+    },
+    clean {
+      js: 'build/javascripts',
+      css: 'build/css',
+      less: 'public/**/*.css'
     }
   });
   // grunt.loadNpmTasks('grunt-contrib-jshint'); now included in require statement
   // grunt.loadNpmTasks('grunt-contrib-less'); now included in require statement
+  // grunt.loadNpmTasks('grunt-contrib-clean'); now included in require statement
   grunt.registerTask('default', ['jshint']);
   grunt.registerTask('js', 'Concatenate and minify static JavaScript assets', ['concat:js', 'uglify:bundle']);
+
 };
+
